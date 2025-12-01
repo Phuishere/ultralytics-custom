f87e08c90ad7d3b6d4daf1e518a0ebcced3c844b
ceenen <phunh.work@gmail.com>
2025-12-01 23:24:00 +0700

    [Update] Incorporating better model custom API

--- ultralytics/nn/custom_tasks.py
+++ ultralytics/nn/custom_tasks.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Union
+from typing import Union, Optional
 from ultralytics.nn.modules import custom_block
 
 CUSTOM_BLOCK_NAMES = frozenset(set([
@@ -8,8 +8,20 @@ CUSTOM_BLOCK_NAMES = frozenset(set([
     if member.__module__ == custom_block.__name__
 ]))
 
-def parse_custom_block_argument(block_name: str, args) -> list[Union[str, int, float]]:
+def parse_custom_block_argument(block_name: str, args: list, ch: Optional[list] = None, f: Optional[int] = None) -> dict[str, Union[str, int, float, list]]:
     if block_name in CUSTOM_BLOCK_NAMES:
-        return args
+        # Get input channel
+        assert ch and f
+        c1 = ch[f]
+        if len(args) > 0: # If has input
+            c2 = args[0]
+        else: # If no input for c2
+            c2 = c1
+        args = args[1:]
+        return {
+            "c1": c1,
+            "c2": c2,
+            "args": args
+        }
     else:
         raise Exception(f"--- ERROR: block_name {block_name} is not included in CUSTOM_BLOCKS {CUSTOM_BLOCK_NAMES}. ---")
